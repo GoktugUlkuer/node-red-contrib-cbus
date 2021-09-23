3c676054a610ca8feca458b005745488bb243d89
Goktug Ulkuer <g.ulkuer@beckhoff.com>
2021-09-23 17:20:59 +0300

    Added IoT SQL node

--- iot-scope-in.js
+++ iot-scope-in.js
@@ -1,7 +1,7 @@
-const mqtt = require('mqtt');
-const fs = require('fs');
-
 module.exports = function (RED) {
+	const mqtt = require('mqtt');
+	const fs = require('fs');
+	
 	function IoTScopeIn(config) {
 		RED.nodes.createNode(this, config);
 
@@ -74,6 +74,7 @@ module.exports = function (RED) {
 				}
 				else {
 					mqttClient.publish(publishTopic, JSON.stringify(json));
+					//console.log("Sending IoT Scope Data");
 					startTime = new Date();
 				}
 			}
@@ -124,4 +125,119 @@ module.exports = function (RED) {
 
 	}
 	RED.nodes.registerType("iot-scope-in", IoTScopeIn);
+	
+	function IoTSql(config) {
+		RED.nodes.createNode(this, config);
+
+		var node = this;
+
+		// Configuration options passed by Node Red
+		node.brokerAddress = config.broker || "127.0.0.1";
+		node.brokerPort = config.brokerport || "1883";
+		node.username = "2ccc6223-64e8-4b96-b6ef-c6fe4c7a7f36";
+		node.password = "c5bcb5c7-6ee0-41de-a502-22953b089d90";
+		node.valueName = config.valuename || "Value Name";
+		node.iotId = config.iot;
+
+		// Config node state
+		var isIotIdValid = node.iotId.length == 16;
+
+		var publishTopic = "CBUS/MQTT/SQL/" + node.iotId;
+		var startTime, endTime;
+
+		startTime = endTime = new Date();
+
+		let options = {
+			username: node.username,
+			password: node.password,
+			connectTimeout: 5000,
+			rejectUnauthorized: false,
+			ca: null,
+			reconnectPeriod: 5000
+		};
+
+		let mqttClient = mqtt.connect(`mqtt://` + node.brokerAddress + ":" + node.brokerPort, options);
+
+		node.status({ fill: "red", shape: "ring", text: "Disconnected" });
+
+
+		node.on('input', function (msg) {
+			if (mqttClient.connected) {
+
+				if (isNaN(msg.payload)) {
+					console.log("input is not a number!");
+					return;
+				};
+
+				if (!isIotIdValid) {
+					console.log("IoT ID is not valid!");
+					return;
+				}
+
+				var json = [
+					{ "Var": "Timestamp", "Val": createTimestamp() },
+					{ "Var": node.valueName, "Val": msg.payload }
+				];
+
+				endTime = new Date();
+
+				var timeDiff = endTime - startTime;
+
+				if (timeDiff < 59500) {
+					console.log("trigger interval is 60 seconds!");
+					return;
+				}
+				else {
+					mqttClient.publish(publishTopic, JSON.stringify(json));
+					//console.log("Sending IoT SQL Data");
+					startTime = new Date();
+				}
+			}
+		});
+
+		function createTimestamp() {
+
+			var date = new Date();
+
+			var month = date.getMonth() + 1;
+			var day = date.getDate();
+			var hour = date.getHours();
+			var min = date.getMinutes();
+			var sec = date.getSeconds();
+
+			month = (month < 10 ? "0" : "") + month;
+			day = (day < 10 ? "0" : "") + day;
+			hour = (hour < 10 ? "0" : "") + hour;
+			min = (min < 10 ? "0" : "") + min;
+			sec = (sec < 10 ? "0" : "") + sec;
+
+			var str = day + "/" + month + "/" + date.getFullYear() + " " + hour + ":" + min + ":" + sec;
+
+			return str;
+		}
+
+		mqttClient.on('connect', function (connack) {
+			if (mqttClient.connected) {
+				console.log("Connected to broker");
+				node.status({ fill: "green", shape: "dot", text: "Connected" });
+			}
+		});
+
+		mqttClient.on('reconnect', () => {
+			console.log('connection reconnecting');
+			node.status({ fill: "yellow", shape: "ring", text: "Reconnecting" });
+		});
+
+		mqttClient.on('offline', () => {
+			console.log('went offline');
+			node.status({ fill: "red", shape: "ring", text: "Disconnected" });
+		});
+
+		mqttClient.on('error', () => {
+			console.log('got error');
+			node.status({ fill: "red", shape: "ring", text: "Disconnected" });
+		});
+
+	}
+	RED.nodes.registerType("iot-sql", IoTSql);
 }
